a50cf3a2848b14c30add014148fbfdc792c223a4
James Bullen <jambun@gmail.com>
2018-10-11 21:12:17 +1100

    First cut frontend kinda done

--- frontend/controllers/history_controller.rb
+++ frontend/controllers/history_controller.rb
@@ -16,4 +16,41 @@ class HistoryController < ApplicationController
   def version
     @version = JSONModel::HTTP.get_json("/history/#{params[:model]}/#{params[:id]}/#{params[:version]}", :mode => 'full')
   end
+
+
+  helper_method :skip_fields
+  def skip_fields
+    ['lock_version', 'created_by', 'last_modified_by', 'create_time', 'system_mtime', 'user_mtime', 'jsonmodel_type', 'uri', 'history']
+  end
+
+  helper_method :time_display
+  def time_display(time)
+    Time.utc(*time.split(/\D+/)[0..5]).getlocal
+  end
+
+
+  helper_method :model_pl
+  def model_pl
+    model = @version['data'].values.first['model']
+    if model.start_with? 'agent_'
+      'agents'
+    elsif model == 'archival_object'
+      'resources'
+    else
+      model.pluralize
+    end
+  end
+
+
+  helper_method :json
+  def json
+    @version['json']
+  end
+
+
+  helper_method :data
+  def data
+    @version['data'].values.first
+  end
+
 end
