cb908d77cdc449e723b988872c15c947917749de
James Bullen <jambun@gmail.com>
2019-01-24 17:23:23 +1100

    make sure the latest version record is there before deleting

--- backend/model/mixins/auditable.rb
+++ backend/model/mixins/auditable.rb
@@ -8,7 +8,19 @@ module Auditable
 
 
   def delete
+    # it is possible that this record is being deleted without
+    # having been retrieved first, in which case there might not
+    # be a version for the latest state of the record
+    # so let's make sure it's there before we delete
+    begin
+      History.new(self.class.table_name.to_s, self.id).version(self.lock_version)
+    rescue History::VersionNotFound
+      # the version will be created when we get the record
+      self.class.to_jsonmodel(self.id)
+    end
+
     super
+
     History.record_delete(self)
   end
 
